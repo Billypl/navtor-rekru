4e24029cd4badf8b671debfd23157a5dca8a6cba
Billypl <kriper01@wp.pl>
2025-05-06 18:52:44 +0200

    Add git-like changes displaying

--- NavtorShiper/Program.cs
+++ NavtorShiper/Program.cs
@@ -1,8 +1,12 @@
-﻿using NavtorShiper.Data;
+﻿using System.Text;
+using DiffPlex.DiffBuilder;
+using DiffPlex;
+using NavtorShiper.Data;
 using NavtorShiper.Entities;
 using NavtorShiper.Repositories;
 using NavtorShiper.Services;
 using NavtorShiper.Utils;
+using DiffPlex.DiffBuilder.Model;
 
 IShipRepository shipRepository = new ShipRepository();
 Seeder seeder = new Seeder(shipRepository);
@@ -11,23 +15,44 @@ seeder.FillShipRepository();
 var tankerShipService = new TankerShipService(shipRepository);
 var passengerShipService = new PassengerShipService(shipRepository);
 
-Console.WriteLine("------ TANKER 1 ------");
-shipRepository.GetAll().Where(s => s is TankerShip).ToList().ForEach(Console.WriteLine);
-tankerShipService.RefuelTank(Seeder.ValidImo1, 1, FuelType.HeavyFuel, 200);
-tankerShipService.RefuelTank(Seeder.ValidImo2, 2, FuelType.Diesel, 300);
-Console.WriteLine("------ TANKER 2 ------");
-shipRepository.GetAll().Where(s => s is TankerShip).ToList().ForEach(Console.WriteLine);
-tankerShipService.EmptyTank(Seeder.ValidImo2, 2);
-Console.WriteLine("------ TANKER 3 ------");
-shipRepository.GetAll().Where(s => s is TankerShip).ToList().ForEach(Console.WriteLine);
-
+PrintTankerOperations();
 Console.WriteLine("------ --------- ------\n");
+PrintPassengerOperations();
 
-Console.WriteLine("------ PASSENGER 1 ------");
-shipRepository.GetAll().Where(s => s is PassengerShip).ToList().ForEach(Console.WriteLine);
-Console.WriteLine("------ PASSENGER 2 ------");
-passengerShipService.AddPassenger(Seeder.ValidImo3, new Passenger(3, "Megan", "Fox"));
-shipRepository.GetAll().Where(s => s is PassengerShip).ToList().ForEach(Console.WriteLine);
-Console.WriteLine("------ PASSENGER 3 ------");
-passengerShipService.RemovePassenger(Seeder.ValidImo3, 1);
-shipRepository.GetAll().Where(s => s is PassengerShip).ToList().ForEach(Console.WriteLine);
+void PrintTankerOperations()
+{
+    Console.WriteLine("------ TANKER 1 ------");
+    var out1 = string.Join('\n', shipRepository.GetAll().OfType<TankerShip>().ToList());
+    Console.WriteLine(out1);
+    
+    Console.WriteLine("------ TANKER 2 ------");
+    tankerShipService.RefuelTank(Seeder.ValidImo1, 1, FuelType.HeavyFuel, 200);
+    tankerShipService.RefuelTank(Seeder.ValidImo2, 2, FuelType.Diesel, 300);
+    string out2 = ShowDiffComparedToCurrent<TankerShip>(shipRepository, out1);
+
+    Console.WriteLine("------ TANKER 3 ------");
+    tankerShipService.EmptyTank(Seeder.ValidImo2, 2);
+    string out3 = ShowDiffComparedToCurrent<TankerShip>(shipRepository, out2);
+}
+
+void PrintPassengerOperations()
+{
+    Console.WriteLine("------ PASSENGER 1 ------");
+    var out1 = string.Join('\n', shipRepository.GetAll().OfType<PassengerShip>().ToList());
+    Console.WriteLine(out1);
+
+    Console.WriteLine("------ PASSENGER 2 ------");
+    passengerShipService.AddPassenger(Seeder.ValidImo3, new Passenger(3, "Megan", "Fox"));
+    string out2 = ShowDiffComparedToCurrent<PassengerShip>(shipRepository, out1);
+
+    Console.WriteLine("------ PASSENGER 3 ------");
+    passengerShipService.RemovePassenger(Seeder.ValidImo3, 1);
+    string out3 = ShowDiffComparedToCurrent<PassengerShip>(shipRepository, out2);
+}
+
+string ShowDiffComparedToCurrent<T>(IShipRepository shipRepository1, string lastState)
+{
+    var newState = string.Join('\n', shipRepository1.GetAll().OfType<T>().ToList());
+    DiffViewr.ShowDiff(lastState, newState);
+    return newState;
+}
---
+++ NavtorShiper/Utils/DiffViewer.cs
@@ -0,0 +1,104 @@
+﻿using DiffPlex.DiffBuilder.Model;
+using DiffPlex.DiffBuilder;
+using DiffPlex;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Pipelines;
+
+namespace NavtorShiper.Utils
+{
+    public class DiffViewr
+    {
+        public static void ShowDiff(string oldText, string newText)
+        {
+            var differ = new Differ();
+            var builder = new SideBySideDiffBuilder(differ);
+            var diff = builder.BuildDiffModel(oldText, newText);
+
+            int lineCount = Math.Max(diff.OldText.Lines.Count, diff.NewText.Lines.Count);
+
+            for (int i = 0; i < lineCount; i++)
+            {
+                var oldLine = i < diff.OldText.Lines.Count ? diff.OldText.Lines[i] : null;
+                var newLine = i < diff.NewText.Lines.Count ? diff.NewText.Lines[i] : null;
+
+
+                if (oldLine?.Type != ChangeType.Deleted 
+                    && newLine?.Type != ChangeType.Inserted
+                    && oldLine?.SubPieces.Count == 0
+                    && newLine?.SubPieces.Count == 0
+                    )
+                {
+                    Console.WriteLine(oldLine?.Text);
+                }
+                else
+                {
+                    WriteDiffLine(oldLine, "left");
+                    WriteDiffLine(newLine, "right");
+                }
+            }
+        }
+
+        private static void WriteDiffLine(DiffPiece? line, string side)
+        {
+            if (line is null || line.Type == ChangeType.Imaginary)
+            {
+                return;
+            }
+            if (line.SubPieces.Count == 0)
+            {
+                WriteDiffByWholeLine(line);
+            }
+            else
+            {
+                WriteDiffByWords(line);
+            }
+        }
+
+        private static void WriteDiffByWords(DiffPiece line)
+        {
+            foreach (var piece in line?.SubPieces)
+            {
+                SetColors(piece.Type);
+                Console.Write(piece.Text);
+                Console.ResetColor();
+            }
+
+            Console.WriteLine();
+        }
+
+        private static void WriteDiffByWholeLine(DiffPiece line)
+        {
+            SetColors(line.Type);
+            Console.Write(line.Text);
+            Console.ResetColor();
+            Console.WriteLine();
+        }
+
+        static void SetColors(ChangeType type)
+        {
+            switch (type)
+            {
+                case ChangeType.Inserted:
+                    Console.BackgroundColor = ConsoleColor.DarkGreen;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                case ChangeType.Deleted:
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                case ChangeType.Modified:
+                    Console.BackgroundColor = ConsoleColor.DarkYellow;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    break;
+                default:
+                    Console.ResetColor();
+                    break;
+            }
+        }
+    }
+}
+    
